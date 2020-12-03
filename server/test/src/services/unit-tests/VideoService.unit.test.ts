6bc3ea99d89915b5d4471701a021bfa7ff288765
JasonLi <jli@mumba.cloud>
2020-12-03 11:48:36 +1100

    set up mocha test

---
+++ server/test/src/services/unit-tests/VideoService.unit.test.ts
@@ -0,0 +1,14 @@
+import { VideoService } from './../../../../src/services/VideoService';
+
+
+describe('VideoService unit test', () => {
+  let instance: VideoService;
+
+  beforeEach(async () => {
+    instance = new VideoService();
+  });
+
+  it('should return videos', async () => {
+
+  });
+});
